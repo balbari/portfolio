69296158362e350fea943e7d1711358b3c43c44f
balbari <anstjswk50@naver.com>
2025-03-25 13:42:57 +0900

    footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <div className='footer' id='contact'>
       <div className='text'>
         <h3>더 궁금한 점이 있다면 아래로 연락주세요!</h3>
-        <p>E-Mail | qwert15023t@gmail.com</p>
+        <p>E-Mail | anstjswk50@naver.com</p>
         <p>Phone | 010-7522-6961</p>
       </div>
       <img src='./img/main.jpg' alt='f_bg' className='f_bg'></img>
