5fce8f858e9a30cfef0dfeefc77e10b2a4f49678
balbari <anstjswk50@naver.com>
2025-03-26 09:25:24 +0900

    line & margin

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,9 +4,12 @@ import '../css/Footer.css'
 function Footer() {
   return (
     <div className='footer' id='contact'>
+      <div className='line'></div>
+      <div className='text'>
         <h3>더 궁금한 점이 있다면 아래로 연락주세요!</h3>
         <p>E-Mail | anstjswk50@naver.com</p>
         <p>Phone | 010-7522-6961</p>
+      </div>
     </div>
   )
 }
